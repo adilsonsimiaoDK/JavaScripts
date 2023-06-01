a6d5cab7bcceecbba25ccee57f9fc5e4d260cf5f
adilsonsimiaoDK <adilsonsimiaock@gmail.com>
2023-06-01 13:48:11 +0200

    part1

---
+++ starter/script.js
@@ -0,0 +1,23 @@
+
+    let js  = 'AmazingJavaScript';
+    console.log(40+41+84);
+    
+let javascript = false;
+console.log(javascript);
+if (javascript) {
+    javascript = false;
+}else{
+    javascript = true;
+
+
+}
+console.log(typeof javascript);
+console.log(typeof 23)
+console.log(typeof "carlas")
+let year;
+year = 1991
+console.log(typeof year);
+
+let age = 30;
+age = 56;
+const birthYear = 1992;
