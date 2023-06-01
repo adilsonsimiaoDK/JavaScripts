d3fd2dc808df69cea5e0231af82cc56b26119547
adilsonsimiaoDK <adilsonsimiaock@gmail.com>
2023-06-01 18:27:06 +0200

    part 2

--- starter/script.js
+++ starter/script.js
@@ -21,11 +21,65 @@
 // let age = 30;
 // age = 56;
 // const birthYear = 1992;
-const now = 2023;
-const ageDuka = now - 1981;
-const ageCissa = now - 1987;
+// const now = 2023;
+// const ageDuka = now - 1981;
+// const ageCissa = now - 1987;
 
-console.log(ageCissa * 2, ageDuka / 10, 2**3);
-const firstName = "Adilson";
-const lastName = "Simiao";
-console.log(firstName +' '+ lastName);
+// console.log(ageCissa * 2, ageDuka / 10, 2**3);
+// // concatanios of string
+// const firstName = "Adilson";
+// const lastName = "Simiao";
+// console.log(firstName +' '+ lastName);
+// // Assignment operators
+// let x = 10;
+// x += 10;
+// x *= 5;
+// x++;
+// x--;
+// x--;
+// console.log(x);
+// // comparison operators
+// console.log(ageCissa <= ageDuka);
+
+/* BMI Categories:
+Underweight = <18.5
+Normal weight = 18.5–24.9
+Overweight = 25–29.9
+Obesity = BMI of 30 or greater
+*/
+
+
+// let weight = 78, height = 1.69;
+
+// BMI = (weight) / (height * height)
+// console.log(BMI.toFixed(1))
+
+// if (BMI <= 18.5){
+//     console.log("Underweight")
+// }
+//     if (BMI > 18.5 && BMI <=24.9) {
+//     console.log("Normal weight")
+// } 
+//     if( BMI >= 24.9 && BMI <=29.9) {
+//     console.log("Overseweight")
+// }   if (BMI >=30.0){
+//     console.log('Obesity')
+// }
+//  const massMark = 95;
+//  const heightMark = 1.88;
+//  const massJohn = 58
+//  const heightJohn = 1.76
+//  const BMIMark = massMark / heightMark ** 2;
+//  const BMIJohn = massJohn / (heightJohn * heightMark);
+//  const markHeigherBMI = BMIMark > BMIJohn;
+//  console.log(BMIMark, BMIJohn, markHeigherBMI);
+
+const firstName = 'Adilson';
+const job = 'teacher';
+const birthYear = 1981;
+const year = 2023;
+const Jonas = " I'm " + firstName + ', a ' + (year - birthYear) + ' years old ' + job + '!';
+console.log(Jonas);
+const newName = `I'm ${firstName}, a ${year -  birthYear} years old ${job}!`;
+console.log(newName)
+console.log(`Just a regular person`);
