810f9793ba9cc4eb1f4777b84e79b6a071568553
adilsonsimiaoDK <adilsonsimiaock@gmail.com>
2023-06-01 18:26:46 +0200

    part2

--- starter/script.js
+++ starter/script.js
@@ -1,23 +1,31 @@
 
-    let js  = 'AmazingJavaScript';
-    console.log(40+41+84);
+//     let js  = 'AmazingJavaScript';
+//     console.log(40+41+84);
     
-let javascript = false;
-console.log(javascript);
-if (javascript) {
-    javascript = false;
-}else{
-    javascript = true;
+// let javascript = false;
+// console.log(javascript);
+// if (javascript) {
+//     javascript = false;
+// }else{
+//     javascript = true;
 
 
-}
-console.log(typeof javascript);
-console.log(typeof 23)
-console.log(typeof "carlas")
-let year;
-year = 1991
-console.log(typeof year);
+// }
+// console.log(typeof javascript);
+// console.log(typeof 23)
+// console.log(typeof "carlas")
+// let year;
+// year = 1991
+// console.log(typeof year);
 
-let age = 30;
-age = 56;
-const birthYear = 1992;
+// let age = 30;
+// age = 56;
+// const birthYear = 1992;
+const now = 2023;
+const ageDuka = now - 1981;
+const ageCissa = now - 1987;
+
+console.log(ageCissa * 2, ageDuka / 10, 2**3);
+const firstName = "Adilson";
+const lastName = "Simiao";
+console.log(firstName +' '+ lastName);
