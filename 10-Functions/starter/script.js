393c27df71872259658d0f349114e5513056f1a0
adilsonsimiaoDK <adilsonsimiaock@gmail.com>
2023-07-11 07:50:21 +0200

    Last review of function

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -286,34 +286,134 @@ GOOD LUCK 😀
 
 
 
-const poll = {
-  question: 'what is your favourite programing language?',
-  option: ['O: JavaScript', '1: Python', '2: Rust', '3:  C++'],
-  answers: new Array(4).fill(0),
+// const poll = {
+//   question: 'what is your favourite programing language?',
+//   option: ['O: JavaScript', '1: Python', '2: Rust', '3:  C++'],
+//   answers: new Array(4).fill(0),
   
   
   
-  registerNewAnswer(){
+//   registerNewAnswer(){
     
-    const answer = Number(
-      prompt(`${this.question}\n ${this.option.join('\n')} \n Write option number`));
-      typeof answer === 'number' && answer < this.answers.length && this.answers[answer]++
+//     const answer = Number(
+//       prompt(`${this.question}\n ${this.option.join('\n')} \n Write option number`));
+//       typeof answer === 'number' && answer < this.answers.length && this.answers[answer]++
       
-     return answer},
+//      return answer},
 
-displayResults(type = 'array'){
-  if (type === 'array'){
-    console.log(this.answers);
-  }else if (type === 'string'){
-    console.log(`Poll results are ${this.answers.join(',')}`);
+// displayResults(type = 'array'){
+//   if (type === 'array'){
+//     console.log(this.answers);
+//   }else if (type === 'string'){
+//     console.log(`Poll results are ${this.answers.join(',')}`);
+//   }
+// },
+// }
+// //const value = prompt("Tapy a value")
+// document.querySelector('.poll').addEventListener('click', poll.registerNewAnswer.bind(poll));
+
+
+
+// poll.displayResults.call({answers: [5,2,3]},"string")
+// poll.displayResults.call({answers: [1,5,2,3, 9,6,1]})
+
+
+// const runOnce = function(){
+//   console.log('This will never run again');
+// }
+// runOnce();
+
+// //IIFE
+// (function(){
+//   console.log('This will never run again');
+//   const isPrivate = 23;
+// })();
+
+
+// (() => console.log('This will also never run again'))();
+
+
+// const secureBooking = function(){
+//   let passengerCount = 0;
+
+// return function(){
+//   passengerCount++;
+//   console.log(`${passengerCount} passengers`);
+// };
+
+// };
+// const booker = secureBooking();
+// booker();
+// booker();
+// booker();
+// console.dir(booker);
+let f;
+const g = function(){
+  const a = 23;
+  f = function(){
+    console.log(a * 2);
   }
-},
 }
-//const value = prompt("Tapy a value")
-document.querySelector('.poll').addEventListener('click', poll.registerNewAnswer.bind(poll));
 
+const h = function(){
+  const b = 777;
+  f = function(){
+    console.log(b * 2);
+  };
 
+}
+g();
+f();
+console.dir(f);
 
-poll.displayResults.call({answers: [5,2,3]},"string")
-poll.displayResults.call({answers: [1,5,2,3, 9,6,1]})
+//Re-assigning f function
+h();
+f();
+console.dir(f);
+ const boardPassenger = function(n, wait){
+  const perGroup =  n / 3
+setTimeout(function()
+{
+  console.log(`We are no bording all ${n} passengers`);
+  console.log(`There are 3 groups, each with  ${perGroup} passengers`);
+}, wait * 1000); 
+console.log(` Will start boarding in ${wait} seconds`);
+};
+//const perGrop 1000;
+boardPassenger(180, 3)
+// Example 2
+const boardPassengers = function (n, wait) {
+  const perGroup = n / 3;
 
+  setTimeout(function () {
+    console.log(`We are now boarding all ${n} passengers`);
+    console.log(`There are 3 groups, each with ${perGroup} passengers`);
+  }, wait * 1000);
+
+  console.log(`Will start boarding in ${wait} seconds`);
+};
+
+const perGroup = 1000;
+boardPassengers(180, 3);
+
+///////////////////////////////////////
+// Coding Challenge #2
+
+/* 
+This is more of a thinking challenge than a coding challenge 🤓
+
+Take the IIFE below and at the end of the function, attach an event listener that changes the color of the selected h1 element ('header') to blue, each time the BODY element is clicked. Do NOT select the h1 element again!
+
+And now explain to YOURSELF (or someone around you) WHY this worked! Take all the time you need. Think about WHEN exactly the callback function is executed, and what that means for the variables involved in this example.
+
+GOOD LUCK 😀
+*/
+(function() {
+  const header = document.querySelector('h1');
+  header.style.color = 'red';
+
+  
+  document.querySelector('body').addEventListener('click', function(){
+    header.style.color = 'blue';
+  });
+})();
